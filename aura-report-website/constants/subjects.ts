4eaec1c4080c73d8b5608215b3ebb01067c9052e
kevin-otter-liu <kevinliusingapore@gmail.com>
2024-12-15 23:10:10 +0800

    fix(build): fix build issues

---
+++ aura-report-website/constants/subjects.ts
@@ -0,0 +1,32 @@
+import { Subject } from "@/types/data/Subject";
+import { nanoid } from "nanoid";
+export const subjects: Subject[] = [
+  {
+    id: nanoid(),
+    name: "E Mathematics",
+  },
+  {
+    id: nanoid(),
+    name: "A Mathematics",
+  },
+  {
+    id: nanoid(),
+    name: "English",
+  },
+  {
+    id: nanoid(),
+    name: "Chinese",
+  },
+  {
+    id: nanoid(),
+    name: "Chemistry",
+  },
+  {
+    id: nanoid(),
+    name: "Physics",
+  },
+  {
+    id: nanoid(),
+    name: "Social Studies",
+  },
+];
